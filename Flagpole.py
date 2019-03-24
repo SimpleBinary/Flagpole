2fae9ae9c7b07182991927c0f99a4afb1158569f
SimpleBinary <40658215+SimpleBinary@users.noreply.github.com>
2019-03-24 11:13:57 +1000

    Update Flagpole.py

--- Flagpole.py
+++ Flagpole.py
@@ -14,7 +14,7 @@ except:
 def image(x):
     if platform.lower() == "win32":
         return userHash - imagehash.average_hash(Image.open(userFlagDir + "\\" + x + ".png")),
-    else
+    else:
         return userHash - imagehash.average_hash(Image.open(userFlagDir + "/" + x + ".png")),
 
 countries = {
@@ -26,10 +26,33 @@ countries = {
                 "Angola": image("Angola"),
                 "Antigua and Barbuda": image("Antigua and Barbuda"),
                 "Argentina": image("Argentina"),
-
+                "Armenia": image("Armenia"),
+                "Artsakh": image("Artsakh"),
+                "Australia": image("Australia"),
+                "Austria": image("Austria"),
+                "Azerbaijan": image("Azerbaijan"),
+                "Bahamas": image("Bahamas"),
+                "Bahrain": image("Bahrain"),
+                "Bangladesh": image("Bangladesh"),
+                "Barbados": image("Barbados"),
+                "Belarus": image("Belarus"),
+                "Belgium": image("Belgium"),
+                "Belize": image("Belize"),
+                "Benin": image("Benin"),
+                "Bhutan": image("Bhutan"),
+                "Bolivia": image("Bolivia"),
+                "Bosnia and Herzegovina": image("Bosnia and Herzegovina"),
+                "Botswana": image("Botswana"),
+                "Brazil": image("Brazil"),
+                "Brunei": image("Brunei"),
+                "Bulgaria": image("Bulgaria"),
+                "Burkina Faso": image("Burkina Faso"),
+                "Burundi": image("Burundi"),
+                "Cambodia": image("Cambodia"),
+                "Cameroon": image("Cameroon"),
             }
 
 print("Calculating...")
 print("Looks Like: " + min(countries, key=countries.get))
-print("Similarity: " + str(countries[min(countries, key=countries.get)]))
+print("Similarity:", countries[min(countries, key=countries.get)][0], "(where 0 is identical)")
 input()
